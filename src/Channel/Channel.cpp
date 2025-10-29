fb771924a277d04672340a519a59220d5b4f2c1f
seungahpyun <seungahpyun@icloud.com>
2025-10-29 12:09:49 +0100

    channel getter

--- src/Channel/Channel.cpp
+++ src/Channel/Channel.cpp
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/10/28 10:31:56 by spyun         #+#    #+#                 */
-/*   Updated: 2025/10/28 13:58:29 by spyun         ########   odam.nl         */
+/*   Updated: 2025/10/29 12:09:31 by seungah       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,49 @@ const std::string& Channel::getName() const
 {
 	return _name;
 }
+
+const std::string& Channel::getTopic() const
+{
+	return _topic;
+}
+
+const std::string& Channel::getKey() const
+{
+	return _key;
+}
+
+size_t Channel::getMemberCount() const
+{
+	return _members.size();
+}
+
+int Channel::getUserLimit() const
+{
+	return _userLimit;
+}
+
+time_t Channel::getCreationTime() const
+{
+	return _creationTime;
+}
+
+bool Channel::isInviteOnly() const
+{
+	return _inviteOnly;
+}
+
+bool Channel::isTopicRestricted() const
+{
+	return _topicRestricted;
+}
+
+bool Channel::hasKey() const
+{
+	return _hasKey;
+}
+
+bool Channel::hasUserLimit() const
+{
+	return _hasUserLimit;
+}
+
