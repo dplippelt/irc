d66d2fd16526950b7fa9823811b54ae89c2b9e57
Takato Mitsuya <tmitsuya@f1r1s6.codam.nl>
2025-11-06 13:14:23 +0100

    Created new branch with Parser and Message class

---
+++ inc/Message.hpp
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Message.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmitsuya <tmitsuya@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 09:29:24 by tmitsuya          #+#    #+#             */
+/*   Updated: 2025/11/06 11:04:05 by tmitsuya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MESSAGE_HPP
+# define MESSAGE_HPP
+
+# include <iostream>
+# include <list>
+# include <sstream>
+# include <string>
+# include <vector>
+
+# define MINIMUM_PARAMS_PASS 1
+# define MINIMUM_PARAMS_NICK 0
+# define MINIMUM_PARAMS_USER 4
+# define MINIMUM_PARAMS_JOIN 1
+# define MINIMUM_PARAMS_MSG 0
+# define MINIMUM_PARAMS_KICK 2
+# define MINIMUM_PARAMS_INVITE 2
+# define MINIMUM_PARAMS_TOPIC 1
+# define MINIMUM_PARAMS_MODE 1
+# define MINIMUM_PARAMS_WHOIS 0
+
+typedef enum e_commands
+{
+	e_pass,
+	e_nick,
+	e_user,
+	e_join,
+	e_msg,
+	e_kick,
+	e_invite,
+	e_topic,
+	e_mode,
+	e_whois,
+	e_unkown,
+}	t_cmds;
+
+class Message
+{
+
+private:
+
+	std::string				m_command{};
+	std::list<std::string>	m_params{};
+	int						m_nparams{};
+	int						m_need_nparams{};
+	int						m_cmd_type{};
+
+	static inline const std::vector<std::string>	k_commands ={
+		"PASS", "NICK", "USER", "JOIN", "PRIVMSG", "KICK", "INVITE", 
+		"TOPIC", "MODE", "WHOIS",
+	};
+
+	void	extractCommand(std::istringstream &message);
+	void	setCommandType();
+	void	setNeedNumParams();
+	void	extractParams(std::istringstream &message);
+
+public:
+
+	Message(const std::string &message);
+	Message(const Message &other);
+	Message	&operator=(const Message &other);
+	~Message();
+
+	void	print() const;
+
+};
+
+#endif
--- inc/Parser.hpp
+++ inc/Parser.hpp
@@ -6,19 +6,20 @@
 /*   By: tmitsuya <tmitsuya@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 13:41:54 by tmitsuya          #+#    #+#             */
-/*   Updated: 2025/11/05 16:37:28 by tmitsuya         ###   ########.fr       */
+/*   Updated: 2025/11/06 11:04:00 by tmitsuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_HPP
 # define PARSER_HPP
 
-# include <string>
-# include <list>
-# include <vector>
-# include <sstream>
 # include <iostream>
+# include <list>
+# include <sstream>
+# include <string>
+# include "Message.hpp"
 
+// To move command action related class
 # define ERR_UNKNOWNCOMMAND 421
 /* 		421     ERR_UNKNOWNCOMMAND
                         "<command> :Unknown command"
@@ -40,74 +41,27 @@
  */
 # define ERR_ALREADYREGISTRED 462
 
-
-typedef enum e_commands
-{
-	e_pass,
-	e_nick,
-	e_user,
-	e_join,
-	e_msg,
-	e_kick,
-	e_invite,
-	e_topic,
-	e_mode,
-	e_whois,
-	e_unkown,
-}	t_cmds;
-
-# define MINIMUM_PARAMS_PASS 1
-# define MINIMUM_PARAMS_NICK 0
-# define MINIMUM_PARAMS_USER 4
-# define MINIMUM_PARAMS_JOIN 1
-# define MINIMUM_PARAMS_MSG 0
-# define MINIMUM_PARAMS_KICK 2
-# define MINIMUM_PARAMS_INVITE 2
-# define MINIMUM_PARAMS_TOPIC 1
-# define MINIMUM_PARAMS_MODE 1
-# define MINIMUM_PARAMS_WHOIS 0
-
-typedef struct s_message
-{
-	std::string				prefix;
-	std::string				command;
-	std::list<std::string>	params;
-	int						nparams;
-	int						cmd_type;
-	int						err_type;
-}	t_message;
-
+// To rename this class to something like 'MessageList'
 class Parser
 {
 
 private:
 
-	std::string				m_buffer{};
-	std::list<t_message>	m_messages{};
+	std::string			m_buffer{};
+	std::list<Message>	m_messages{};
 
-	static inline const std::vector<std::string>	k_commands ={
-		"PASS", "NICK", "USER", "JOIN", "PRIVMSG", "KICK", "INVITE", 
-		"TOPIC", "MODE", "WHOIS",
-	};
-
-	void	partitioning(const std::string &input);
-	void	partPrefixAndCommand(std::istringstream &message, t_message &elem);
-	void	partParams(std::istringstream &message, t_message &elem);
-	void	setParams(std::istringstream &message, t_message &elem, int nim);
-	void	setCommandType(t_message &elem);
-	// void	paramsValidation(t_message &elem);
+	void	parse();
 
 public:
 
 	Parser();
 	Parser(const Parser &other);
-	const Parser	&operator=(const Parser &other);
+	Parser	&operator=(const Parser &other);
 	~Parser();
 
-	Parser	&loadInput(const std::string &input);
-	Parser	&parse();
-	std::list<t_message>	&getMessages();
-	void	print() const;
+	Parser				&load(const std::string &input);
+	std::list<Message>	&getMessages();
+	void				print() const;
 };
 
 #endif
